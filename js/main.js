ee6687d36d5f70947aa7e0865741bd88a9edf3ff
Andrew Quackenbos <andrew@sitesquad.net>
2013-03-11 01:51:55 -0400

    General update

--- js/main.js
+++ js/main.js
@@ -7,39 +7,67 @@ function randomize()
 {
 	$("#results").empty();
 	
-	$(".filter-entry").each(function(idx,r){
+	$(".filter-group").each(function(gidx,r){
 		var row = $(r);
-		var identifier = row.find("input[id*='identifier-']").val();
-		var type = row.find("select[id*='type-']").val();
-		var options = window.availableFilters[identifier].options;
-		var value = "";
 		
-		if(type == 'none') return true;
+		var results = [];
+		if(!window.availableFilters[gidx].filters) return true;
 		
-		var limiter = $.makeArray(row.find("select[id*='options-']").val());
+		row.find(".filter-entry").each(function(idx,entry){
 		
-		if(type == 'limited' || type == 'set')
-		{
-			options = $.map(options, function(item, idx) {
-				return (limiter.indexOf(item.value) != -1) ? item : null;
-			});
+			var type = $(this).find("select.type").val();
+			var options = window.availableFilters[gidx].filters[idx].options;
+			
+			if(type == 'none') return true;
+			
+			var limiter = $.makeArray($(this).find("select.options").val());
+			
+			if(type == 'limited' || type == 'set')
+			{
+				options = $.map(options, function(item, idx) {
+					return (limiter.indexOf(item.value) != -1) ? item : null;
+				});
+			}
+			var selected = options[Math.floor(Math.random() * options.length)];
+			
+			var result = {};
+			
+			result.group = window.availableFilters[gidx].identifier;
+			result.identifier = selected.value;
+			result.label = $(this).find("label").html();
+			result.value = selected.label;
+			result.image = "img/"+result.group+"/"+result.identifier+".png";
+			
+			results.push(result);
+		});
+		
+		var groupResult = {
+			identifier: window.availableFilters[gidx].identifier,
+			label: window.availableFilters[gidx].label,
+			results: results
 		}
 		
-		value = options[Math.floor(Math.random() * options.length)].label;
-		
-		var result = {};
-		
-		result.label = row.find("label").html();
-		result.value = value;
-		
-		var template = Handlebars.compile($("#template-result").html());
-		$(template(result).trim()).hide().appendTo("#results").delay( idx * 250 ).animate({
+		var template = Handlebars.compile($("#template-group-result").html());
+		$(template(groupResult).trim()).hide().appendTo("#results").delay( gidx * 250 ).animate({
 			"height": "toggle", "opacity": "toggle" 
-			}, 250);;
+			}, 250);
 	});
 }
 
 $(function(){
+	window.groupDefaults = {
+		"identifier": "",
+		"label": "",
+		"filters": []
+	};
+
+	window.filterDefaults = {
+		"identifier": "",
+		"label": "",
+		"disallow": [],
+		"options": []
+	};
+
 	window.selectOptions = [
 		{"value":"random","label":"Random"},
 		{"value":"limited","label":"Limited (select)"},
@@ -58,11 +86,65 @@ $(function(){
 		return new Handlebars.SafeString(result);
 	});
 
+	Handlebars.registerHelper('renderFilters', function(filters, group) {
+		var output = "";
+		
+		$.each(filters, function(idx,filter){
+			filter = $.extend({},window.filterDefaults,filter);
+		
+			if((idx%3) == 0) output += '<div class="row-fluid">';
+		
+			var template = Handlebars.compile($("#template-filter").html());
+			output += template($.extend(filter,{"group":group})).trim();
+			
+			if((idx+1)%3 == 0) output += '</div>';
+		});
+		
+		if(filters.length%3 == 0)
+			output += "</div>";
+		
+		return new Handlebars.SafeString(output);
+	});
+	
+	Handlebars.registerHelper('renderResults', function(results) {
+		var output = "";
+		
+		$.each(results, function(idx, result) {
+			if((idx%3) == 0) output += '<div class="row-fluid">';
+		
+			var template = Handlebars.compile($("#template-result").html());
+			output += template(result).trim();
+			
+			//Begin image check process
+			$.ajax({
+				url: result.image,
+				type:'HEAD',
+				error:
+					function(){
+						
+						$("#result-"+result.group+"-"+result.identifier+" img").attr('src','img/no-image.png').fadeIn();
+					},
+				success:
+					function(){
+						$("#result-"+result.group+"-"+result.identifier+" img").attr('src',result.image).fadeIn();
+					}
+			});
+			
+			if((idx+1)%3 == 0) output += '</div>';
+		});
+		
+		if(results.length%3 == 0)
+			output += "</div>";
+		
+		return new Handlebars.SafeString(output);
+	});
+	
 	$.getJSON("data.json",function(data){
 		window.availableFilters = data;
-		$.each(data, function(idx,filter){
-			var template = Handlebars.compile($("#template-filter").html());
-			$(template(filter).trim()).hide().appendTo("#filterArea").delay( idx * 250 ).animate({
+		$.each(data, function(idx,group){
+			group = $.extend({},window.groupDefaults,group);
+			var template = Handlebars.compile($("#template-group").html());
+			$(template(group).trim()).hide().appendTo("#filterArea").delay( idx * 250 ).animate({
 				"height": "toggle", "opacity": "toggle" 
 				}, 250);;
 		});
@@ -73,6 +155,11 @@ $(function(){
 	
 	$("#btn-randomize").on("click",function(e){
 		e.preventDefault();
+		$($(".filter-group").get().reverse()).each(function(idx) {
+			$(this).delay( idx * 250 ).animate({
+				"height": "toggle", "opacity": "toggle" 
+				}, 250);
+		});
 		$($(".filter-entry").get().reverse()).each(function(idx) {
 			$(this).delay( idx * 250 ).animate({
 				"height": "toggle", "opacity": "toggle" 
@@ -91,6 +178,11 @@ $(function(){
 	
 	$("#btn-refilter").on("click",function(e){
 		e.preventDefault();
+		$(".filter-group").each(function(idx) {
+			$(this).delay( idx * 250 ).animate({
+				"height": "toggle", "opacity": "toggle" 
+				}, 250);
+		});
 		$(".filter-entry").each(function(idx) {
 			$(this).delay( idx * 250 ).animate({
 				"height": "toggle", "opacity": "toggle" 
@@ -102,8 +194,8 @@ $(function(){
 		resetResults();
 	});
 	
-	$("body").on("change","select[id*='type-']",function(e){
-		var optList = $(this).parent().find("select[id*='options-']");
+	$("body").on("change","select.type",function(e){
+		var optList = $(this).parent().find("select.options");
 		optList.show();
 		switch($(this).val()){
 			case "limited":
